21ac82f76cb98eebdc68a9b5822071d9972227a4
raoufBA <raouf.belanes@gmail.com>
2021-11-24 08:51:37 +0100

    config sfDoctrineGuardPlugin

---
+++ apps/backend/modules/default/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * default actions.
+ *
+ * @package    jobeet
+ * @subpackage default
+ * @author     Your name here
+ * @version    SVN: $Id$
+ */
+class defaultActions extends sfActions
+{
+  /**
+   * Executes index action
+   *
+   * @param sfRequest $request A request object
+   */
+  public function executeIndex(sfWebRequest $request)
+  {
+//    $this->forward('default', 'module');
+  }
+}
